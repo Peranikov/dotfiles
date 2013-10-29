5cabdd6e12fd8377c76c3155bf2f2f597a194132
matsukubo <matsukubo@irvinesystems.co.jp>
2013-10-29 11:29:10 +0900

    add .vimrc

--- setup.sh
+++ setup.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-DOT_FILES=(.zshrc)
+DOT_FILES=(.zshrc .vimrc)
 
 for file in ${DOT_FILES[@]}
 do
     ln -s $HOME/dotfiles/$file $HOME/$file
 done
-
-# install oh-my-zsh
-[! -d ~/.oh-my-zsh ] && git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
